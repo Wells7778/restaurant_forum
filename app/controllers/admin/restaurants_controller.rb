1b91e23f23b662143a200a770580fa6a931b3a76
Wells <chawkeye@gmail.com>
2017-11-17 11:11:13 +0800

    Added new & create method to Admin::Restaurants controller & View

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,8 +1,33 @@
 class Admin::RestaurantsController < ApplicationController
+# t.string "name"
+# t.string "tel"
+# t.string "address"
+# t.string "opening_hours"
+# t.text "description"
+
   before_action :authenticate_user!
   before_action :authenticate_admin
   def index
     @restaurants = Restaurant.all
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to admin_root_path, notice: '建立餐廳資料完成'
+    else
+      render :new, notice: '建立餐廳資料失敗'
+    end
+  end
+
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :tel, :address, :opening_hours, :description)
+  end
 end
